35502394ac1f90d96286fc3f5ea78c9ad5fc05e9
DEMONT Christophe (SNCF RESEAU / EIC PARIS NORD / Equipe Bobigny) <8201953s@commun.ad.sncf.fr>
2022-05-17 12:44:31 +0200

    Added Sound

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -3,3 +3,21 @@ function lancer_son() {
   audio.volume = 1; // on met le son au maximum
   audio.play(); // on lance le son
 }
+
+function son2() {
+  let audio2 = document.getElementById("MonSon2");
+  audio2.volume = 1;
+  audio2.play();
+}
+
+function son3() {
+  let audio3 = document.getElementById("MonSon3");
+  audio3.volume = 1;
+  audio3.play();
+}
+
+function son4() {
+  let audio4 = document.getElementById("MonSon4");
+  audio4.volume = 1;
+  audio4.play();
+}
